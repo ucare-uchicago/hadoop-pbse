32d79214d7180da5b240a9f6ebeaff46b5a359c1
castuardo <castuardo@uchicago.edu>
2016-08-25 12:00:37 -0700

    Added fix for reduce progress

--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core/src/main/java/org/apache/hadoop/mapred/Task.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core/src/main/java/org/apache/hadoop/mapred/Task.java
@@ -811,33 +811,30 @@ abstract public class Task implements Writable, Configurable{
             datanodeRetries = 0;
           }
       
-          // @Cesar: Only relevant for reduce tasks
-          if(!isMapTask()){
-	          // @Cesar: Send progress every 60000 / proginterval seconds for reduce phase
-	          // or until we have fetch rate reports or reduce pipeline
-	          int sendReduceProgress = !isMapTask() ? 60000 / proginterval : 0;
-	          boolean shouldSendShuffleProgress = (taskStatus == null ||
-	        		  							   taskStatus.getReportedFetchRates() == null || 
-						 						   taskStatus.getReportedFetchRates().getFetchRateReport() == null || 
-						 						   taskStatus.getReportedFetchRates().getFetchRateReport().size() == 0);
-	          boolean shouldSendHdfsWRiteProgress = (this.out == null);
-	          
-	          if (sendReduceProgress > 0 && (shouldSendHdfsWRiteProgress? shouldSendShuffleProgress : false)) {
-	        	  sendReduceProgress -= 1;
-	        	  if(LOG.isDebugEnabled()){
-		              LOG.debug("@Cesar: Not sending progress since flag is " + sendReduceProgress + " and " + 
-		            		  	"(shouldSendHdfsWRiteProgress=" + shouldSendHdfsWRiteProgress + ", " + 
-		            		  	"shouldSendShuffleProgress=" + shouldSendShuffleProgress + ")");
-	        	  }
-	              sendProgress = sendProgress || resetProgressFlag();
-	              continue;
-	            } else {
-	            	sendReduceProgress = 0;
-	            	if(LOG.isDebugEnabled()){
-	            		LOG.debug("@Cesar: Time to send progress for reduce task!");
-	            	}
-	            }
-          }
+       // @Cesar: Send progress every 60000 / proginterval seconds for reduce phase
+          // or until we have fetch rate reports or reduce pipeline
+          int sendReduceProgress = !isMapTask() ? 60000 / proginterval : 0;
+          boolean shouldSendShuffleProgress = (taskStatus == null ||
+        		  							   taskStatus.getReportedFetchRates() == null || 
+					 						   taskStatus.getReportedFetchRates().getFetchRateReport() == null || 
+					 						   taskStatus.getReportedFetchRates().getFetchRateReport().size() == 0);
+          boolean shouldSendHdfsWRiteProgress = (this.out == null);
+          
+          if (sendReduceProgress > 0 && (shouldSendHdfsWRiteProgress? shouldSendShuffleProgress : false)) {
+        	  sendReduceProgress -= 1;
+        	  if(LOG.isDebugEnabled()){
+	              LOG.debug("@Cesar: Not sending progress since flag is " + sendReduceProgress + " and " + 
+	            		  	"(shouldSendHdfsWRiteProgress=" + shouldSendHdfsWRiteProgress + ", " + 
+	            		  	"shouldSendShuffleProgress=" + shouldSendShuffleProgress + ")");
+        	  }
+              sendProgress = sendProgress || resetProgressFlag();
+              continue;
+            } else {
+            	sendReduceProgress = 0;
+            	if(LOG.isDebugEnabled() && !isMapTask()){
+            		LOG.debug("@Cesar: Time to send progress for reduce task!");
+            	}
+            }
           
           
           //huanke
