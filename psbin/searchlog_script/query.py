27ac19c07f6e6f87fa48da8891d95df923c3f9b1
Riza Suminto <riza.on@gmail.com>
2016-08-29 16:26:18 -0500

    edit genstats.py to ver 2.3

--- psbin/searchlog_script/query.py
+++ psbin/searchlog_script/query.py
@@ -1,15 +1,27 @@
 import table
 
-# Search speculative map that read from slow datanode 
-# FROM = table.MAPS
-# WHERE = lambda map: not map['attempt'].endswith('_0') and (map['isSlowDatanode'] == True)
-# SELECT = lambda map: [map['attempt'], map['mapnode'], map['lastDatanode']]
+''' Search speculative map that read from slow datanode 
+FROM = table.MAPS
+WHERE = lambda map: not map['attempt'].endswith('_0') and (map['isSlowDatanode'] == True)
+SELECT = lambda map: [map['attempt'], map['mapnode'], map['lastDatanode']] '''
 
-# Search job latency of job that involve slow node
-print "appid, duration(s), #maps, #reduce, slowDN, slowMap, slowReduce, mapsTopo"
+
+
+''' Search job that (involve slownode and run for more than 80s) or had PBSE algorithm triggered
+print "appid, duration(s), #maps, #reduce, slowDN, slowMap, slowReduce, pbseTags, mapsTopo"
 FROM = table.JOBS
-WHERE = lambda job: job["isInvolveSlownode"] and float(job["job_duration"]) > 180
+WHERE = lambda job: (job["isInvolveSlownode"] and float(job["job_duration"]) > 80) or job["tags_PBSE"]!=[]
 SELECT = lambda job: [job["appid"][-3:], job["job_duration"], job["ct_CompletedMaps"], job["ct_CompletedReds"],\
                       job["slowNodeInvolvedInDataread"], job["slowNodeInvolvedInMap"], job["slowNodeInvolvedInReduce"],\
+                      " ".join(tag[5:] for tag in job["tags_PBSE"]),\
                       " ".join(['%s:%s' % (key, value) for (key, value) in job["mapsTopo"].items()])]
-ORDERBY = lambda job: (job["job_duration"])
+ORDERBY = lambda job: (job["job_duration"]) '''
+
+
+
+''' Search all attempt by task id '''
+FROM = table.TASKS
+WHERE = lambda task: task["appid"].endswith("_0051")
+SELECT = lambda task: [task["attempt"], task["lastDatanode"], task["mapnode"], \
+                       task["reducenode"], task["topo"] if "topo" in task else "--"]
+ORDERBY = lambda task: task["attempt"]
