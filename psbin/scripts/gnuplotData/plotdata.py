f257dc6b3a084c8ae0f335b6e8d231bde63ae8e6
Riza Suminto <riza.on@gmail.com>
2016-09-11 21:52:11 -0500

    add script to make gnuplot data

---
+++ psbin/scripts/gnuplotData/plotdata.py
@@ -0,0 +1,141 @@
+#!/usr/bin/python
+
+import os, re, sys
+import pprint
+import datetime
+import json
+from collections import defaultdict
+
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib.backends.backend_pdf import PdfPages
+import matplotlib.pyplot as plt
+
+pp = pprint.PrettyPrinter(indent=2)
+
+
+NORMALIZE = False
+CDF_Y_LIMIT = 1.0
+
+TASKS = lambda apps: [task for aname,a in apps.items() \
+           for tname,task in a["containers"].items()]
+MAPS = lambda apps: [a for a in TASKS(apps) if (a["ismap"])]
+REDUCES = lambda apps: [a for a in TASKS(apps) if (not a["ismap"])]
+AM = lambda apps: [a["master"] for aname,a in apps.items()]
+JC = lambda apps: [a["jobclient"] for aname,a in apps.items() if ("jobclient" in a)]
+ALL_JOBS = lambda apps: [a for aname,a in apps.items()]
+
+
+def getTaskId(ct):
+  att = ct["attempt"]
+  return att[8:35]
+
+
+def getLogTime(line):
+  datestr = re_date.search(line).group(0)
+  thedate = datetime.datetime.now()
+  if "-" in datestr:
+    thedate = datetime.datetime.strptime(datestr, "%Y-%m-%d %H:%M:%S,%f")
+  else:
+    thedate = datetime.datetime.strptime(datestr, "%y/%m/%d %H:%M:%S")
+
+  return thedate.strftime("%Y-%m-%d %H:%M:%S.%f")
+
+
+def strToDate(datestr):
+  return datetime.datetime.strptime(datestr, "%Y-%m-%d %H:%M:%S.%f")
+
+
+def makeCDFPoints(vals):
+  X = np.asarray(sorted(vals))
+  Y = np.ones(len(vals))
+  Y /= Y.sum()
+  Y = np.cumsum(Y)
+#  X = np.insert(X,0,0)
+#  Y = np.insert(Y,0,0)
+  if CDF_Y_LIMIT < 1.0:
+    TX = []
+    TY = []
+    for i in xrange(0,len(X)):
+      if (Y[i] <= CDF_Y_LIMIT):
+        TX.append(X[i])
+        TY.append(Y[i])
+    X = np.array(TX)
+    Y = np.array(TY)
+  return (X,Y)
+
+
+def printData(runs, selector, prefix):
+  for runid,data in sorted(runs.items(), key=lambda x:x[0]):
+    dat = selector(data["apps"])
+    X,Y = makeCDFPoints(dat)
+
+    f = open(prefix+"-"+runid+".dat", 'w+')
+    for i in xrange(0,len(X)):
+      f.write("%f\t%f\n" % (X[i],Y[i]))
+    f.close()
+
+
+def printAllRunData(runs):
+  # Job Running Time
+  dat = lambda apps: \
+        [(strToDate(a["time_stop"])-strToDate(a["time_start"])) .total_seconds() \
+         for a in AM(apps) if ("time_start" in a) and ("time_stop" in a)]
+  printData(runs,dat,"am")
+
+
+def printDiff(d1,d2,filename):
+  diff = []
+  for k,v in d1.items():
+    diff.append(v-d2[k])
+  X,Y = makeCDFPoints(diff)
+  f = open(filename, 'w+')
+  for i in xrange(0,len(X)):
+    f.write("%f\t%f\n" % (X[i],Y[i]))
+  f.close()
+
+
+def printDiffData(json1,json2,prefix):
+  dat = lambda apps: \
+        [(a["appid"][-3:],(strToDate(a["time_stop"])-strToDate(a["time_start"])) .total_seconds()) \
+         for a in AM(apps) if ("time_start" in a) and ("time_stop" in a)]
+  d1 = {k:v for k,v in dat(json1["apps"])}
+  d2 = {k:v for k,v in dat(json2["apps"])}
+  printDiff(d1,d2,"spdup-"+prefix+".dat")
+  
+  
+
+def loadJson(fname):
+  with open(fname) as data_file:
+    json_data = json.load(data_file)
+    fileversion = json_data["conf"]["version"]
+    return json_data
+
+
+def loadExpStats():
+  runs = {}
+  for root, dirnames, filenames in os.walk('.'):
+    for fname in filenames:
+      if fname.endswith(".json"):
+        runs[fname.replace(".json","")] = loadJson(fname)
+    break
+  return runs
+
+
+def main():
+  opt = sys.argv[1]
+  if opt == "-a":
+    runs = loadExpStats()
+    printAllRunData(runs)
+  elif opt == "-d":
+    run1 = sys.argv[2]
+    run2 = sys.argv[3]
+    prefix = sys.argv[4]
+    json1 = loadJson(run1)  
+    json2 = loadJson(run2)
+    printDiffData(json1,json2,prefix)  
+
+
+if __name__ == '__main__':
+  main()
