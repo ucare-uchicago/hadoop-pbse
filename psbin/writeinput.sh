e594ac52754dabad543e0e8b0aeb72993ab54226
Riza Suminto <riza.on@gmail.com>
2017-01-09 10:47:11 -0700

    replace config early in writeinput phase

--- psbin/writeinput.sh
+++ psbin/writeinput.sh
@@ -3,12 +3,12 @@
 . cluster_topology.sh
 
 ssh -t $YARN_RM "clstop;"
+cp ucare_se_conf/writeconf/* $HADOOP_CONF_DIR/
+sed_replaceconf.sh
 clcleanlogs
 clreset
 clsetup
 ssh -t $HDFS_NN "dfsformat;"
-cp ucare_se_conf/writeconf/* $HADOOP_CONF_DIR/
-sed_replaceconf.sh
 
 ssh -t $YARN_RM "clstart;sleep 10;"
 ssh -t $CLIENT_NODE "prep_run.sh > /tmp/writeinput.log &;"
